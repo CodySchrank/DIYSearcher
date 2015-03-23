09b3473333bee0e2b42db4da937c2a7a5b9e724c
Cody Schrank <cody.schrank@gmail.com>
2015-03-23 15:36:56 -0700

    Added files

---
+++ application/views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DIY SEARCHER</title>
+	<link rel="stylesheet" type="text/css" href="/assets/skeleton/normalize.css">
+	<link rel="stylesheet" type="text/css" href="/assets/skeleton/skeleton.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/header.css">
+</head>
+<body>
+<div class="container">
+	<? require('partials/header.php') ?>
+</div>
+</body>
+</html>
---
+++ application/views/partials/header.php
@@ -0,0 +1,13 @@
+<header class="row">
+	
+	<h2>DIYSearcher</h2>
+
+	<a href="/popular">Popular</a>
+
+	<a href="/browse/all">Browse All</a>
+
+	<a href="/browse/new">New Postings</a>
+
+	
+
+</header>
