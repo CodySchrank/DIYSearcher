9de2609309d1536270cca8010f8e7ad83f6d4fea
Cody Schrank <cody.schrank@gmail.com>
2015-03-24 08:45:48 -0700

    Updated Index

--- application/views/index.php
+++ application/views/index.php
@@ -2,10 +2,27 @@
 <html>
 <head>
 <? require('partials/head.php') ?>
+<link rel="stylesheet" type="text/css" href="/assets/css/index.css">
 </head>
 <body>
 	<? require('partials/header.php') ?>
 <div class="container">
+<div class="row">
+	<h2>Find Your Next Project</h2>
+</div>
+<div class="row">
+	<div class="five columns">
+		<a href="/browse" id="browse-projects"></a>
+		<p>Browse Projects</p>
+	</div>
+	<div class="two columns">
+		Or
+	</div>
+	<div class="five columns">
+		<a id="find-projects"></a>
+		<p>Find Projects</p>
+	</div>
+</div>
 </div>
 </body>
 </html>
