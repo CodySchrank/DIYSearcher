09d1973de0f364c87dd65cedd4ec174f479a6bc8
Arash Namvar <namvar.arash@gmail.com>
2015-03-24 08:44:52 -0700

    working ont the project page

---
+++ application/views/project.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php require("partials/head.php") ?>
+	<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="/assets/css/project.css">
+</head>
+<body>
+<?php require("partials/header.php") ?>
+		<div class="project-picture">
+			<div class="container">
+				<div class="row">
+					<div class="two columns author">
+						<img class="creator" src="/assets/pics/whale.jpg">
+						<h6>By <a href="#">Arash Namvar</a></h6>
+					</div>
+					<div class="text-background ten columns">
+						<h1>VHS video toaster</h1>
+					</div>					
+				</div>
+			</div>
+		</div>
+	<div class="container">
+		<div class="row description">
+			<h5>The project was simple: convert a VHS video machine to make toast, and eject it through the cassette slot. semiotics Portland. Ennui organic DIY blog four dollar toast, next level VHS cardigan banh mi bicycle rights fingerstache stumptown butcher. Small batch kogi 3 wolf moon narwhal lomo whatever. Gluten-free four loko Williamsburg, VHS 3 wolf moon McSweeney's Wes Anderson migas fixie Truffaut. DIY kale chips locavore synth.
+
+</h5>
+		</div>
+		<div class="row ratings">
+			<span>Difficulty: </span><span class="bold moderate"> Moderate</span>
+			<span>$</span><span class="light-dollar">$$$</span>
+			<span >Overall Rating:</span> <span class="bold">10/10</span>
+		</div>
+		<div class="row">
+			<div class="six columns video">
+				<embed src="http://www.youtube.com/v/fqQz_CBQKhw" type="application/x-shockwave-flash"wmode="transparent">
+			</div>
+			<div class="six columns">
+				
+		</div>
+
+	</div>
+
+</body>
+</html>
