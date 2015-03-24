84b1635e30236c96da3bc2c4d46827b252945c92
Cody Schrank <cody.schrank@gmail.com>
2015-03-24 14:38:13 -0700

    Finished header

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -15,6 +15,12 @@
 
 	<a href="/checkout" id="checkout"></a>
 
-	<a href="/profile" id="profile"></a>
+	<button id="profile">
+	<ul>
+		<li><a href="/signin">Sign In</a></li>
+		<li><a href="/register">Register</a></li>
+		<li><a href="/diy">Create DIY</a></li>
+	</ul>
+	</button>
 	</div>
 </header>
