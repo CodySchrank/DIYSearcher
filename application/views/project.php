13c06ab47ab39586a447734fac14e82e646752e8
Arash Namvar <namvar.arash@gmail.com>
2015-03-24 14:23:55 -0700

    finished project page

--- application/views/project.php
+++ application/views/project.php
@@ -62,7 +62,180 @@
 			</div>
 		</div>
 
-	</div>
+		<div class="row steps-seciton1">
+			<div class="four columns">
+				<img class="step1" src="/assets/pics/toast1.jpg">
+				<a class="button step-button" href="">See More Images</a>
+			</div>
+			<div class="eight columns">
+				<h3>STEP 1: Parts list</h3>
+				<p>
+				VHS video recorder/player - I used a Daewoo ST862P
+				Toaster - I used a Swan Elegance single slot "thick & thin"
+				Metal sheet - From steel drinks cans
+				Epoxy glue - (cheap, two part)
 
+				Tools:
+				Dremel-a-like tool 
+				Pliers
+				A sharp knife
+				Sharp scissors
+				A screwdriver
+				</p>
+			</div>
+		</div>
+		<div class="row steps-seciton1">
+			<div class="four columns">
+				<img class="step1" src="/assets/pics/toast2.jpg">
+				<a class="button step-button" href="">See More Images</a>
+			</div>
+			<div class="eight columns">
+				<h3>STEP 2: Toaster dismemberment</h3>
+				<p>
+				This toaster was not designed to come apart, but the plastic case must come off. In this step I stripped off what I didn't want and wasn't going to use <br>
+
+On the underside are Torx screws for which I did not have bits. The two prominent screws were removed with pliers, the others had the brown plastic hacked away from them until I could finish the job with pliers. <br>
+
+The two control knobs are friction-fits on this machine and only required a good pull to get them off <br>
+
+Whilst doing this I observed that the outer shell is rather hard and must have good heat resistance. To some extent the toaster self-cools by natural convection, the last photograph shows the the comparison between inner and outer. <br>
+
+I wasn't going to be able to rely on this type of cooling or plastic in the video. <br>
+				</p>
+			</div>
+		</div>
+		<div class="row steps-seciton1">
+				<div class="four columns">
+					<img class="step1" src="/assets/pics/toast3.jpg">
+					<a class="button step-button" href="">See More Images</a>
+				</div>
+				<div class="eight columns">
+					<h3>STEP 3: Toaster dismemberment (part 2)</h3>
+					<p>
+					Once you're past the outer case on this toaster everything gets much easier. Most of the construction is held together by metal tabs, bent or twisted to secure. This is the stuff I need. <br>
+
+The electrical connections to the heater elements were push fits, the pulled off with pliers. Take care with this type of heater element, the mica support is fragile.<br>
+
+I needed to take the whole thing apart because the toaster has a central slot, the video has an off-centre slot. While it wasn't possible to bring the toaster fully up to the level of the VHS slot, it could be moved a bit.<br>
+					</p>
+				</div>
+			</div>
+		<div class="row steps-seciton1">
+				<div class="four columns">
+					<img class="step1" src="/assets/pics/toast4.jpg">
+					<a class="button step-button" href="">See More Images</a>
+				</div>
+				<div class="eight columns">
+					<h3>Step 4: Video dismemberment</h3>
+					<p>
+					The video was much easier to strip than the toaster, a few screws and I was done.<br>
+
+The front panel was a clip-fit, easy-on, easy-off.<br>					
+					</p>
+				</div>
+			</div>
+			<div class="row steps-seciton1">
+				<div class="four columns">
+					<img class="step1" src="/assets/pics/toast5.jpg">
+					<a class="button step-button" href="">See More Images</a>
+				</div>
+				<div class="eight columns">
+					<h3>Step 5: Toaster adaption</h3>
+					<p>
+					It was apparent from the start that this would be a tight fit, so some of the video's black side-wall had to go (Dremel-a-like tool).<br>	
+Having tested the toaster without it's case, I didn't think it a good idea to leave a sheet of black polystyrene under it, and some bits of this would have to be removed anyway (Dremel-a-like).<br>	
+There's a thin steel sheet underneath the plastic floor.
+
+The toaster has two chrome racks which hinge at the bottom, and are pushed central at the top so that they grip the bread in a central position - at varying thicknesses.<br>	
+
+I fixed the top rack at it's thickest setting, and moved the bottom rack to "very thin". The overall effect is to move the toast off-centre and more towards the top in the finished machine.<br>					
+					</p>
+				</div>
+			</div>
+			<div class="row steps-seciton1">
+				<div class="four columns">
+					<img class="step1" src="/assets/pics/toast6.jpg">
+					<a class="button step-button" href="">See More Images</a>
+				</div>
+				<div class="eight columns">
+					<h3>Step 6: Assembly of the VHS toaster</h3>
+					<p>
+					It was apparent from the start that this would be a tight fit, so some of the video's black side-wall had to go (Dremel-a-like tool).<br>	
+Having tested the toaster without it's case, I didn't think it a good idea to leave a sheet of black polystyrene under it, and some bits of this would have to be removed anyway (Dremel-a-like).<br>	
+There's a thin steel sheet underneath the plastic floor.
+
+The toaster has two chrome racks which hinge at the bottom, and are pushed central at the top so that they grip the bread in a central position - at varying thicknesses.<br>	
+
+I fixed the top rack at it's thickest setting, and moved the bottom rack to "very thin". The overall effect is to move the toast off-centre and more towards the top in the finished machine.<br>					
+					</p>
+				</div>
+			</div>
+			<div class="row steps-seciton1">
+				<div class="four columns">
+					<img class="step1" src="/assets/pics/toast7.jpg">
+					<a class="button step-button" href="">See More Images</a>
+				</div>
+				<div class="eight columns">
+					<h3>Step 7: The finished product!</h3>
+					<p>
+					Three shots of the finished machine.<br>
+
+It does toast, see the video. <br>
+
+Yes it gets hot, yes I want to fit at least one fan before I use it again<br>
+
+L<br>					
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<h5>COMMENTS</h5>
+			</div>
+			<div class="row steps-seciton1">
+				<div class="two columns">
+					<img class="comment-img" src="/assets/pics/10yearold.jpg">
+				</div>
+				<div class="ten columns comment">
+					<h3>I freaking loved this project!</h3>
+					<p>
+							<p>All of my middle school friends and I worked on this project in mrs. thompson's class. It was da bommmmbbb!!</p>	
+					</p>
+				</div>
+			</div>
+			<div class="row steps-seciton1">
+				<div class="two columns">
+					<img class="comment-img" src="/assets/pics/10yearold.jpg">
+				</div>
+				<div class="ten columns comment">
+					<h3>I freaking loved this project!</h3>
+					<p>
+							<p>All of my middle school friends and I worked on this project in mrs. thompson's class. It was da bommmmbbb!!</p>	
+					</p>
+				</div>
+			</div>
+			<div class="row steps-seciton1">
+				<div class="two columns">
+					<img class="comment-img" src="/assets/pics/10yearold.jpg">
+				</div>
+				<div class="ten columns comment">
+					<h3>I freaking loved this project!</h3>
+					<p>
+							<p>All of my middle school friends and I worked on this project in mrs. thompson's class. It was da bommmmbbb!!</p>	
+					</p>
+				</div>
+			</div>
+			<div class="row steps-seciton1">
+				<div class="two columns">
+					<img class="comment-img" src="/assets/pics/10yearold.jpg">
+				</div>
+				<div class="ten columns comment">
+					<h3>I freaking loved this project!</h3>
+					<p>
+							<p>All of my middle school friends and I worked on this project in mrs. thompson's class. It was da bommmmbbb!!</p>	
+					</p>
+				</div>
+			</div>
+	</div>
+<?php require("partials/footer.php") ?>
 </body>
 </html>
