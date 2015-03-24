4ab42683bbbf7909353f0365ff618e0ba67330db
Arash Namvar <namvar.arash@gmail.com>
2015-03-23 20:19:34 -0700

    updated browse

--- application/views/browse.php
+++ application/views/browse.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>DIY Searcher Browse</title>
+	<?php require("partials/head.php") ?>
 	<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="/assets/css/skeleton/skeleton.css">
-	<link rel="stylesheet" type="text/css" href="/assets/css/skeleton/normalize.css">
 	<link rel="stylesheet" type="text/css" href="/assets/css/browse.css">
 </head>
 <body>
+<?php require("partials/header.php") ?>
 	<div class="container">	
 		<h6>Showing Results for: <a class="results">tech</a> , <a class="results"> hacker</a></h6>
 		<div class="row">
 			<div class="two columns">
 			</div>
 			<div class="ten columns">
-						
 				<ul class="sort-by">
 					<span class="sort-title">SORT BY:</span>
 					<li class="sort-by-border"><a href="">Newest</a></li>
@@ -27,7 +25,7 @@
 		</div>
 		<div class="row project-tile">
 			<img class="project-picture three columns" src="/assets/pics/pipad1.jpg">
-			<div class="nine columns">
+			<div class="eight columns">
 				<h5><a href="#">PiPad</a></h5> <span>By</span> <a class="author"href="">Michael K Castor</a>
 				<p class="">Learn how to build a PiPad. It's essentially an iPad but without the evil Apple iOS. This means you actually have freedom. </p>
 				<span>Difficulty: </span><span class="bold moderate"> Moderate</span>
@@ -38,7 +36,7 @@
 
 		<div class="row project-tile">
 			<img class="project-picture three columns" src="/assets/pics/dogfan.jpg">
-			<div class="nine columns">
+			<div class="eight columns">
 				<h5><a href="#">Redneck air conditioned dog bed</a></h5> <span>By</span> <a class="author"href="">Perry Jenkins</a>
 				<p class="">My blonde lab is 13 years old and digs up my flower bed to get to the cool dirt on hot days. She makes a huge mess of things. So I built this air conditioned bed for her to lay on.</p>
 				<span>Difficulty: </span><span class="bold moderate"> Easy</span>
@@ -47,10 +45,10 @@
 			</div>
 		</div>
 
-		
+
 		<div class="row project-tile">
 			<img class="project-picture three columns" src="/assets/pics/dogfan.jpg">
-			<div class="nine columns">
+			<div class="eight columns">
 				<h5><a href="#">Redneck air conditioned dog bed</a></h5> <span>By</span> <a class="author"href="">Perry Jenkins</a>
 				<p class="">My blonde lab is 13 years old and digs up my flower bed to get to the cool dirt on hot days. She makes a huge mess of things. So I built this air conditioned bed for her to lay on.</p>
 				<span>Difficulty: </span><span class="bold moderate"> Easy</span>
