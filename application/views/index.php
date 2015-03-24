647cb7bc5bdea11495086cb366c7492a6f5d2779
Cody Schrank <cody.schrank@gmail.com>
2015-03-24 13:24:51 -0700

    Random lols

--- application/views/index.php
+++ application/views/index.php
@@ -5,12 +5,20 @@
 <link rel="stylesheet" type="text/css" href="/assets/css/index.css">
 </head>
 <body>
-	<? require('partials/header.php') ?>
+	<? require('partials/header_index.php') ?>
+<img id="background" src='/assets/pics/index-background.jpg'>
 <div class="container">
-<div class="row">
-	<h2>Find Your Next Project</h2>
+<div class="row search">
+	<h2>Tagline</h2>
+	<form action="/search" method="post">
+		<input type="text" name="search">
+		<input type="submit" value="Search">
+	</form>
 </div>
 <div class="row">
+	<h2>Discover Your Next Project</h2>
+</div>
+<div class="row discover">
 	<div class="five columns">
 		<a href="/browse" id="browse-projects"></a>
 		<p>Browse Projects</p>
@@ -23,6 +31,20 @@
 </div>
 <div class="row popular">
 	<h3>Popular DIY Projects</h3>
+</div>
+<div class="row">
+	<div class="four columns">
+		<h4>Title</h4>
+		<p>Description</p>
+	</div>
+	<div class="four columns">
+		<h4>Title</h4>
+		<p>Description</p>
+	</div>
+	<div class="four columns">
+		<h4>Title</h4>
+		<p>Description</p>
+	</div>
 </div>
 </div>
 </body>
---
+++ application/views/partials/header_index.php
@@ -0,0 +1,16 @@
+<header class="row">
+	
+	<h2>DIYSearcher</h2>
+
+	<a href="/popular">Popular</a>
+
+	<a href="/browse/all">Browse All</a>
+
+	<a href="/browse/new">New Postings</a>
+	<div>
+
+	<a href="/checkout" id="checkout"></a>
+
+	<a href="/profile" id="profile"></a>
+	</div>
+</header>
