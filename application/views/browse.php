019f6e386a649d99422df149e1c6427f2f001ef3
Arash Namvar <namvar.arash@gmail.com>
2015-03-23 19:22:04 -0700

    browse half way done

--- application/views/browse.php
+++ application/views/browse.php
@@ -2,20 +2,67 @@
 <html>
 <head>
 	<title>DIY Searcher Browse</title>
+	<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="/assets/css/skeleton/skeleton.css">
 	<link rel="stylesheet" type="text/css" href="/assets/css/skeleton/normalize.css">
 	<link rel="stylesheet" type="text/css" href="/assets/css/browse.css">
 </head>
 <body>
-
-	<div style="width:100%; height:50px; border:1px black solid"></div>
-
 	<div class="container">	
-		<h6>Showing Results for: INSERT SEARCH STUFF HERE</h6>
-		<div class="result-tile">
-			<img class="result-tile-img" src="/assets/pics/pipad1.jpg">
+		<h6>Showing Results for: <a class="results">tech</a> , <a class="results"> hacker</a></h6>
+		<div class="row">
+			<div class="two columns">
+			</div>
+			<div class="ten columns">
+						
+				<ul class="sort-by">
+					<span class="sort-title">SORT BY:</span>
+					<li class="sort-by-border"><a href="">Newest</a></li>
+					<li class="sort-by-border"><a href="">Hottest</a></li>
+					<li class="sort-by-border"><a href="">Reccommended</a></li>
+					<li class="sort-by-border"><a href="">Price Low-to-High</a></li>
+					<li><a href="">Price High-to-Low</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row project-tile">
+			<img class="project-picture three columns" src="/assets/pics/pipad1.jpg">
+			<div class="nine columns">
+				<h5><a href="#">PiPad</a></h5> <span>By</span> <a class="author"href="">Michael K Castor</a>
+				<p class="">Learn how to build a PiPad. It's essentially an iPad but without the evil Apple iOS. This means you actually have freedom. </p>
+				<span>Difficulty: </span><span class="bold moderate"> Moderate</span>
+				<span>$$$</span><span class="light-dollar">$</span>
+				<span >Overall Rating:</span> <span class="bold">7/10</span>
+			</div>
+		</div>
+
+		<div class="row project-tile">
+			<img class="project-picture three columns" src="/assets/pics/dogfan.jpg">
+			<div class="nine columns">
+				<h5><a href="#">Redneck air conditioned dog bed</a></h5> <span>By</span> <a class="author"href="">Perry Jenkins</a>
+				<p class="">My blonde lab is 13 years old and digs up my flower bed to get to the cool dirt on hot days. She makes a huge mess of things. So I built this air conditioned bed for her to lay on.</p>
+				<span>Difficulty: </span><span class="bold moderate"> Easy</span>
+				<span>$$</span><span class="light-dollar">$$</span>
+				<span >Overall Rating:</span> <span class="bold">1/10</span>
+			</div>
+		</div>
+
+		
+		<div class="row project-tile">
+			<img class="project-picture three columns" src="/assets/pics/dogfan.jpg">
+			<div class="nine columns">
+				<h5><a href="#">Redneck air conditioned dog bed</a></h5> <span>By</span> <a class="author"href="">Perry Jenkins</a>
+				<p class="">My blonde lab is 13 years old and digs up my flower bed to get to the cool dirt on hot days. She makes a huge mess of things. So I built this air conditioned bed for her to lay on.</p>
+				<span>Difficulty: </span><span class="bold moderate"> Easy</span>
+				<span>$$</span><span class="light-dollar">$$</span>
+				<span >Overall Rating:</span> <span class="bold">1/10</span>
+			</div>
 		</div>
 	</div>
+
+	
+	</div>
+
 
 
 
